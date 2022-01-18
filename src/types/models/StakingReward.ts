67430caf6cb45f3246e670d7191bf0e3d6237821
rolikov <olegrolik@gmail.com>
2022-01-18 14:17:17 +0300

    refactoring, optimize imports

--- src/types/models/StakingReward.ts
+++ src/types/models/StakingReward.ts
@@ -2,15 +2,10 @@
 import {Entity, FunctionPropertyNames} from "@subql/types";
 import assert from 'assert';
 
-
-
-
 export class StakingReward implements Entity {
-
     constructor(id: string) {
         this.id = id;
     }
-
 
     public id: string;
 
@@ -22,33 +17,31 @@ export class StakingReward implements Entity {
 
     public blockHeight: number;
 
-
-    async save(): Promise<void>{
+    async save(): Promise<void> {
         let id = this.id;
         assert(id !== null, "Cannot save StakingReward entity without an ID");
         await store.set('StakingReward', id.toString(), this);
     }
-    static async remove(id:string): Promise<void>{
+
+    static async remove(id: string): Promise<void> {
         assert(id !== null, "Cannot remove StakingReward entity without an ID");
         await store.remove('StakingReward', id.toString());
     }
 
-    static async get(id:string): Promise<StakingReward | undefined>{
+    static async get(id: string): Promise<StakingReward | undefined> {
         assert((id !== null && id !== undefined), "Cannot get StakingReward entity without an ID");
         const record = await store.get('StakingReward', id.toString());
-        if (record){
+        if (record) {
             return StakingReward.create(record);
-        }else{
+        } else {
             return;
         }
     }
 
-
-
     static create(record: Partial<Omit<StakingReward, FunctionPropertyNames<StakingReward>>> & Entity): StakingReward {
         assert(typeof record.id === 'string', "id must be provided");
         let entity = new StakingReward(record.id);
-        Object.assign(entity,record);
+        Object.assign(entity, record);
         return entity;
     }
 }
